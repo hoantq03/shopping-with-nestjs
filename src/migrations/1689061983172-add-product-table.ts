c2df64c8941bf2ddd05bb01f9a9c785be74c4525
Qhoan_Tran3008 <thaihoang03082003@gmail.com>
2023-07-11 18:44:27 +0700

    [DEV] add product and categories database (better version x2)

--- src/migrations/1689061983172-add-product-table.ts
+++ src/migrations/1689061983172-add-product-table.ts
@@ -12,13 +12,15 @@ export class AddProductTable1689061983172 implements MigrationInterface {
         "imageUrl" character varying(20) NOT NULL,
         "price" real NOT NULL,
         "categoryId" character varying NOT NULL,
+        "userId" character varying NOT NULL,
         "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
         "created_by" character varying NOT NULL DEFAULT '1',
         "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
         "updated_by" character varying NOT NULL DEFAULT '1',
         
         CONSTRAINT "PK_a3ffb1c0c8416b9fc6b7433f907" PRIMARY KEY ("id"),
-        CONSTRAINT "FK_a3fb435cfb170c8416b9fc6f907" FOREIGN KEY ("categoryId") REFERENCES categories("categoryId")
+        CONSTRAINT "FK_a3fb435cfb170c8416b9fc6f907" FOREIGN KEY ("categoryId") REFERENCES categories("categoryId"),
+        CONSTRAINT "FK_a3fb435c6f907fb170c8416b9fc" FOREIGN KEY ("userId") REFERENCES users("id")
         );
         `);
   }
