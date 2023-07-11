8c04a1b1c13a065956cbc1e71f84a3c62d84c6ba
Qhoan_Tran3008 <thaihoang03082003@gmail.com>
2023-07-11 18:36:41 +0700

    [DEV] add product and categories database (better version)

--- src/migrations/1689061983172-add-product-table.ts
+++ src/migrations/1689061983172-add-product-table.ts
@@ -5,19 +5,20 @@ export class AddProductTable1689061983172 implements MigrationInterface {
     queryRunner.query(`
     CREATE TABLE "products" (
         "id" character varying NOT NULL,
-        name character varying(100) NOT NULL,
-        description character varying NOT NULL,
-        color character varying(100) NOT NULL,
-        discount character varying NOT NULL,
-        imageUrl character varying(20) NOT NULL,
-        categoryId character varying NOT NULL,
-        created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-        created_by character varying NOT NULL DEFAULT '1',
-        updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-        updated_by character varying NOT NULL DEFAULT '1',
+        "name" character varying(100) NOT NULL,
+        "description" character varying NOT NULL,
+        "color" character varying(100) NOT NULL,
+        "discount" character varying NOT NULL,
+        "imageUrl" character varying(20) NOT NULL,
+        "price" real NOT NULL,
+        "categoryId" character varying NOT NULL,
+        "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+        "created_by" character varying NOT NULL DEFAULT '1',
+        "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+        "updated_by" character varying NOT NULL DEFAULT '1',
         
-        CONSTRAINT "PK_a3ffb1c0c8416b9fc6b7433f907" PRIMARY KEY (id),
-        CONSTRAINT "FK_a3fb435cfb170c8416b9fc6f907" FOREIGN KEY (categoryId) REFERENCES categories(categoryId)
+        CONSTRAINT "PK_a3ffb1c0c8416b9fc6b7433f907" PRIMARY KEY ("id"),
+        CONSTRAINT "FK_a3fb435cfb170c8416b9fc6f907" FOREIGN KEY ("categoryId") REFERENCES categories("categoryId")
         );
         `);
   }
--- src/migrations/1689062464491-add-category-table.ts
+++ src/migrations/1689062464491-add-category-table.ts
@@ -5,15 +5,15 @@ export class AddCategoryTable1689062464491 implements MigrationInterface {
     queryRunner.query(`
     CREATE TABLE "categories" (
         "categoryId" character varying NOT NULL,
-        name character varying(100) NOT NULL,
-        description character varying NOT NULL,
+        "name" character varying(100) NOT NULL,
+        "description" character varying NOT NULL,
 
-        created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-        created_by character varying NOT NULL DEFAULT '1',
-        updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-        updated_by character varying NOT NULL DEFAULT '1',
+        "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+        "created_by" character varying NOT NULL DEFAULT '1',
+        "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+        "updated_by" character varying NOT NULL DEFAULT '1',
         
-        CONSTRAINT "PK_a3ffb1c0c8416b9fc6b7433saf4" PRIMARY KEY (categoryId)
+        CONSTRAINT "PK_a3ffb1c0c8416b9fc6b7433saf4" PRIMARY KEY ("categoryId")
 
         );
         `);
